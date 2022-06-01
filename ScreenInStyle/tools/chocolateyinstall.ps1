e0ddc4de66fd2c94dbe3d9eac736ab1de71423fb
fmotzet <106673224+fmotzet@users.noreply.github.com>
2022-06-01 16:48:58 +0200

    Add files via upload

---
+++ ScreenInStyle/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.eizoglobal.com/support/db/files/software/software/lcd/ScreenInStyle/1.1.10_client/SIS1110_Setup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = https://www.eizoglobal.com/support/db/files/software/software/lcd/ScreenInStyle/1.1.10_client/SIS1110_Setup.exe
+
+  softwareName  = 'ScreenInStyle*'
+
+  checksum      = 'CBEC37FBDC7646F65BDCF667B57F9CA841051551F13A9AC6F7B474996B0734DF'
+  checksumType  = 'sha256'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
