a477da13cbdaecbc81b962bc1b0dcd164df5509b
Felix Motzet <f.motzet@campus.lmu.de>
2023-12-08 20:53:58 +0100

    update to 1.1.13

--- ScreenInStyle/tools/chocolateyinstall.ps1
+++ ScreenInStyle/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-#$url        = 'https://www.eizoglobal.com/support/db/files/software/software/lcd/ScreenInStyle/1.1.10_client/SIS1110_Setup.exe'
+#$url        = 'https://www.eizoglobal.com/support/db/files/software/software/lcd/ScreenInStyle/1.1.13_client/SIS1113_Setup.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = 'https://www.eizoglobal.com/support/db/files/software/software/lcd/ScreenInStyle/1.1.10_client/SIS1110_Setup.exe'
+  url           = 'https://www.eizoglobal.com/support/db/files/software/software/lcd/ScreenInStyle/1.1.13_client/SIS1113_Setup.exe'
 
   softwareName  = 'ScreenInStyle*'
 
-  checksum      = 'CBEC37FBDC7646F65BDCF667B57F9CA841051551F13A9AC6F7B474996B0734DF'
+  checksum      = 'e7ef990f415493abda43b1c77d6094492694c25996ae70d4e648a65d7d626352'
   checksumType  = 'sha256'
 
   validExitCodes= @(0)
