ff885ffe4e83c2a7a1c5a582263536a1ed58ffab
fmotzet <felix.motzet@gmail.com>
2025-01-22 17:39:16 +0100

    updated the package to version 1.1.16

--- ScreenInStyle/tools/chocolateyinstall.ps1
+++ ScreenInStyle/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = 'https://www.eizoglobal.com/support/db/files/software/software/lcd/ScreenInStyle/1.1.13_client/SIS1113_Setup.exe'
+  url           = 'https://www.eizoglobal.com/support/db/files/software/software/lcd/ScreenInStyle/1.1.16_client/SIS1116_Setup.exe'
 
   softwareName  = 'ScreenInStyle*'
 
-  checksum      = 'e7ef990f415493abda43b1c77d6094492694c25996ae70d4e648a65d7d626352'
+  checksum      = '12c85b98a42523972219a831f3dcbf0ea094ac30928c9361f75fa802340a678d'
   checksumType  = 'sha256'
 
   validExitCodes= @(0)
